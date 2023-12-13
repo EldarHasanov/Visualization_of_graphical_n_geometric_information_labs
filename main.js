11d679b527431eeb3e63dd1850b733c9160b8428
EldarHasanov <eldargasanovtr91@gmail.com>
2023-12-13 20:13:35 +0200

    Adding second lab

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function deg2rad(angle) {
 function Model(name) {
     this.name = name;
     this.iVertexBuffer = gl.createBuffer();
+    this.iNormalBuffer = gl.createBuffer();
     this.count = 0;
 
     this.BufferData = function (vertices) {
@@ -25,13 +26,24 @@ function Model(name) {
         this.count = vertices.length / 3;
     }
 
+    this.NormalBufferData = function (vertices) {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+        this.count = vertices.length / 3;
+    }
+
     this.Draw = function () {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        gl.drawArrays(gl.TRIANGLES, 0, this.count);
     }
 }
 
@@ -81,14 +93,47 @@ function draw() {
 
     gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
 
+    let normalMatrix = m4.identity();
+    m4.inverse(modelView, normalMatrix);
+    m4.transpose(normalMatrix, normalMatrix);
+
+    gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
+
     /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
+    gl.uniform4fv(shProgram.iColor, [1, 1, 1, 1]);
+
+    let diff = document.getElementById('dC').value;
+    let spec = document.getElementById('sC').value;
+    let ambi = document.getElementById('aC').value;
+
+    console.log(diff)
+    console.log(spec)
+    console.log(ambi)
+
+    let dif = hex2rgb(diff)
+    let spe = hex2rgb(spec)
+    let amb = hex2rgb(ambi)
+
+    console.log(dif)
+    console.log(spe)
+    console.log(amb)
+
+    gl.uniform3fv(shProgram.iAmbientColor, [amb.r/255, amb.g/255, amb.b/255]);
+    gl.uniform3fv(shProgram.iDiffuseColor, [dif.r/255, dif.g/255, dif.b/255]);
+    gl.uniform3fv(shProgram.iSpecularColor, [spe.r/255, spe.g/255, spe.b/255]);
+    let kA = document.getElementById('kA').value;
+    let kD = document.getElementById('kD').value;
+    let kS = document.getElementById('kS').value;
+    gl.uniform1f(shProgram.ikA, kA);
+    gl.uniform1f(shProgram.ikD, kD);
+    gl.uniform1f(shProgram.ikS, kS);
 
     surface.Draw();
 }
 
 function cloverKnotVerts(U_NUM_STEPS, V_NUM_STEPS, R, a) {
     let vertices = []
+    let normals = []
     const U_MAX = 12 * PI
     const V_MAX = 2 * PI
     const U_STEP = U_MAX / U_NUM_STEPS
@@ -96,9 +141,20 @@ function cloverKnotVerts(U_NUM_STEPS, V_NUM_STEPS, R, a) {
     for (let u = 0; u < U_MAX; u += U_STEP) {
         for (let v = 0; v < V_MAX; v += V_STEP) {
             vertices.push(...cloverKnot(u, v, R, a))
+            normals.push(...normalAnalytic(u, v, R, a))
+            vertices.push(...cloverKnot(u + U_STEP, v, R, a))
+            normals.push(...normalAnalytic(u + U_STEP, v, R, a))
+            vertices.push(...cloverKnot(u, v + V_STEP, R, a))
+            normals.push(...normalAnalytic(u, v + V_STEP, R, a))
+            vertices.push(...cloverKnot(u, v + V_STEP, R, a))
+            normals.push(...normalAnalytic(u, v + V_STEP, R, a))
+            vertices.push(...cloverKnot(u + U_STEP, v, R, a))
+            normals.push(...normalAnalytic(u + U_STEP, v, R, a))
+            vertices.push(...cloverKnot(u + U_STEP, v + V_STEP, R, a))
+            normals.push(...normalAnalytic(u + U_STEP, v + V_STEP, R, a))
         }
     }
-    return vertices;
+    return [vertices, normals];
 }
 
 function cloverKnot(u, v, R, a) {
@@ -107,6 +163,21 @@ function cloverKnot(u, v, R, a) {
     let y = (R + a * cos(u / 2)) * sin(u / 3) + a * sin(u / 3) * cos(v - PI);
     let z = a + sin(u / 2) + a * sin(v - PI);
     return [m * x, m * y, m * z];
+}
+
+const e = 0.0001;
+function normalAnalytic(u, v, R, a) {
+    let u1 = cloverKnot(u, v, R, a),
+        u2 = cloverKnot(u + e, v, R, a),
+        v1 = cloverKnot(u, v, R, a),
+        v2 = cloverKnot(u, v + e, R, a);
+    const dU = [], dV = []
+    for (let i = 0; i < 3; i++) {
+        dU.push((u1[i] - u2[i]) / e)
+        dV.push((v1[i] - v2[i]) / e)
+    }
+    const n = m4.normalize(m4.cross(dU, dV))
+    return n
 }
 
 
@@ -118,19 +189,32 @@ function initGL() {
     shProgram.Use();
 
     shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
     shProgram.iColor = gl.getUniformLocation(prog, "color");
+    shProgram.ikA = gl.getUniformLocation(prog, "kA");
+    shProgram.ikD = gl.getUniformLocation(prog, "kD");
+    shProgram.ikS = gl.getUniformLocation(prog, "kS");
+    shProgram.iAmbientColor = gl.getUniformLocation(prog, "ambientColor");
+    shProgram.iDiffuseColor = gl.getUniformLocation(prog, "diffuseColor");
+    shProgram.iSpecularColor = gl.getUniformLocation(prog, "specularColor");
 
     surface = new Model('Surface');
-    document.getElementById('nU').onchange = (e) => {
-        let newnU = parseInt(document.getElementById('nU').value)
-        surface.BufferData(cloverKnotVerts(newnU, 100, 2, 0.5));
-        draw()
-    }
-    let nU = parseInt(document.getElementById('nU').value)
-    surface.BufferData(cloverKnotVerts(nU, 100, 2, 0.5));
+    let bufferDataCK = cloverKnotVerts(100, 100, 2, 0.5)
+    surface.BufferData(bufferDataCK[0]);
+    surface.NormalBufferData(bufferDataCK[1]);
 
     gl.enable(gl.DEPTH_TEST);
+}
+
+const hex2rgb = (hex) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    // return {r, g, b} 
+    return { r, g, b };
 }
 
 
